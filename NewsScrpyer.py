513c9f0a3c40df01fcdf288dd98a97cb05dba8da
ylin <lyzhou@cn.ibm.com>
2018-05-12 23:23:20 +0800

    remove comments from BatchJob. It may be necessary to rename the Main class.

--- NewsScrpyer.py
+++ NewsScrpyer.py
@@ -18,12 +18,38 @@ class NewsScrpyer:
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0',
     }
+    dom = None
 
-    def tagWithMostP(self, dom):
+    def getDom(self, url):
         import bs4
-        for child in dom.children:
-            print(child.get('name'))
-        #dom = bs4.BeautifulSoup(page, "html.parser")
+        import requests
+
+        page = requests.get(url, headers=self.headers).content.replace('\n', '').replace(
+            '\t', '').replace('\r', '').replace('\xa0', '')
+        self.dom = bs4.BeautifulSoup(page, "html.parser")
+        return self.dom
+
+    def tagWithMostP(self, dom, tagMaxP=None):
+        for child in dom.findChildren(recursive=False):
+            if child and not child.get('name') == 'p':
+                numMaxP = 0
+
+                if tagMaxP:
+                    numMaxP = len(tagMaxP.find_all('p', recursive=False))
+
+                numCurrentP = len(child.find_all('p', recursive=False))
+                if numCurrentP > numMaxP:
+                    tagMaxP = child
+
+                tagMaxP = self.tagWithMostP(dom=child, tagMaxP=tagMaxP)
+        return tagMaxP
+
+    def getContent(self, dom=None):
+        if dom is None:
+            dom = self.dom
+
+        return self.tagWithMostP(dom).text
+        # dom = bs4.BeautifulSoup(page, "html.parser")
 
 
 def Main():
@@ -33,10 +59,9 @@ def Main():
     import requests
     import bs4
     # print(scryper.login(user='g07xw6@163.com', passwd='Bonnie123.'))
-    url = 'http://www.xinhuanet.com/house/2018-04-23/c_1122723902.htm'
-    page = requests.get(url, headers=scryper.headers).content
-    dom = bs4.BeautifulSoup(page, "html.parser")
-    scryper.tagWithMostP(dom)
+    url = 'http://blog.sina.com.cn/s/blog_7d75a1df0102y1y7.html?tj=1'
+    dom = scryper.getDom(url)
+    print(scryper.getContent(dom))
 
 
 Main()
